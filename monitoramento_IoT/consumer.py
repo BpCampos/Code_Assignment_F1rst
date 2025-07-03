a1e18fdaa12342289e205ad883fe7180fe8118fe
BpCampos <campos.bruno98@gmail.com>
2025-07-03 20:52:18 -0300

    update: instala bibliotecas pyodbc e dotenv para respectivamente inserir dados no banco de dados e carregar a variavel de ambiente

--- monitoramento_IoT/consumer.py
+++ monitoramento_IoT/consumer.py
@@ -1,8 +1,22 @@
 from quixstreams import Application
+import pyodbc
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Application(broker_address="localhost:9092")
 
 topic = app.topic(name="iot-data", value_serializer="json")
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=localhost,1433;"
+    "DATABASE=iot_data;"
+    "UID=sa;"
+    f"PWD={os.getenv("SA_PASSWORD")}"
+)
+cursor = conn.cursor()
 
 with app.get_consumer() as consumer:
 
